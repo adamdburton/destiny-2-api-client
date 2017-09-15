662db45bbcf40e0e57a8eb6d102b1e33cf8cae68
adamdburton <adam@burt0n.net>
2017-09-15 06:13:32 +0100

    php-http/client-common requirement

--- src/Api/Client.php
+++ src/Api/Client.php
@@ -2,12 +2,17 @@
 
 namespace AdamDBurton\Destiny2ApiClient\Api;
 
+use Http\Discovery\MessageFactoryDiscovery;
+
 class Client
 {
 	protected $client;
 
 	public function __construct()
 	{
-
+		$this->client = client = new HttpMethodsClient(
+			HttpClientDiscovery::find(),
+			MessageFactoryDiscovery::find()
+		);
 	}
 }
